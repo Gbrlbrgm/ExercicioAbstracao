06f399e33bc2557a5087136a998e6fde2fc928fa
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2021-04-14 15:11:39 -0300

    Projeto Concluido

---
+++ Abstract/Entities/Enums/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract.Entities.Enums
+{
+    enum Color
+    {
+        Black,
+        Blue,
+        Red
+    }
+}
---
+++ Abstract/Entities/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abstract.Entities.Enums;
+
+namespace Abstract.Entities
+{
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+
+    
+}
